ed35c20efbc2864ac6c56bbf3039c7d510a98a02
Taha Khaled <tahakhaledabdellah@gmail.com>
2023-12-16 02:26:08 +0200

    feat : image config

--- next.config.js
+++ next.config.js
@@ -2,6 +2,17 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  images: {
+    formats: ["image/avif", "image/webp"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "forsabackend.blob.core.windows.net",
+        port: "",
+        pathname: "/media/**",
+      },
+    ],
+  },
 };
 
 module.exports = nextConfig;
